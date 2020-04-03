2cd12bbce57b7cb07f45f4dca3179c39dfd22e71
Igor Popov <ipkroks@gmail.com>
2020-04-04 01:56:41 +0300

    create math ant test

---
+++ src/models/Math.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\models;
+
+
+/**
+ * Class Math
+ * @package app\models
+ */
+class Math
+{
+    public function mypow($a, $b)
+    {
+        return $a * $b;
+    }
+}
---
+++ tests/models/MathTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tests\models;
+
+use PHPUnit\Framework\TestCase;
+use app\models\Math;
+
+/**
+ * Class MathTest
+ * @package tests\models
+ */
+class MathTest extends TestCase
+{
+    public function testGetName(){
+        $model = new Math;
+        $this->assertEquals(3, $model->mypow(2, 3));
+    }
+}
