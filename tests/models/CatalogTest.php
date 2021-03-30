55e7f1882330adfd4ac05a5d2424cf20cfadd0b3
Igor Popov <ipkroks@gmail.com>
2021-03-30 14:24:48 +0300

    upd catalog

--- tests/models/CatalogTest.php
+++ tests/models/CatalogTest.php
@@ -12,12 +12,12 @@ use app\models\Catalog;
 class CatalogTest extends TestCase
 {
     public function testGetName(){
-        $model = new Catalog(1, 'Роутер');
-        $this->assertEquals($model->getName(), 'Роутер');
+        $model = new Catalog(1, 'Репитер');
+        $this->assertEquals($model->getName(), 'Репитер');
     }
 
     public function testGetId(){
-        $model = new Catalog(13, 'Роутер');
+        $model = new Catalog(13, 'Репитер');
         $this->assertEquals($model->getId(), 13);
     }
 }
